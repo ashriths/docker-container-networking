6ebd099a3028bf35fd141df021f275b49eb9d2c8
Ashrith Sheshan <ashrith.sheshan@gmail.com>
2018-02-16 20:09:46 -0800

    Add qperf install script

---
+++ scripts/qperf-install-ubuntu.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y make gcc libc-dev
+wget https://www.openfabrics.org/downloads/qperf/qperf-0.4.9.tar.gz
+tar xvf qperf-0.4.9.tar.gz
+cd qperf-0.4.9 && ./configure && make
+echo "qperf is now available at `pwd`/qperf-0.4.9/src/qperf"
