d1f13d5acbe2a52469420d408e569a4d5d8587d5
Ashrith Sheshan <ashrith.sheshan@gmail.com>
2018-02-25 15:19:06 -0800

    Add repeater shell script

---
+++ scripts/repeater.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function execute_command {
+    echo "Executing: $@"
+    eval $@ | tee -a "$1.output"
+    rc=$?
+    echo "Done"
+    echo
+    return $rc
+}
+
+NAME=$0;
+COUNT=$1;
+shift;
+i=0
+while [ "$i" -le "$COUNT" ];
+do
+    echo "$i"
+    execute_command $@
+    i=$(($i + 1))
+    sleep 1
+done
