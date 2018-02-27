00fd8f4b2996204996fd2206bf344d9e8ecdff64
Ashrith Sheshan <ashrith.sheshan@gmail.com>
2018-02-27 12:13:13 -0800

    add 100s tcp results and script

---
+++ scripts/run_tcp_window_tests.sh
@@ -0,0 +1,22 @@
+set -ex
+iperf3 -c c10-35.sysnet.ucsd.edu -w 2KB -t 100 
+iperf3 -c c10-35.sysnet.ucsd.edu -w 4KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 8KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 12KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 16KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 20KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 24KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 28KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 32KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 36KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 40KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 44KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 48KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 52KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 56KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 60KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 64KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 68KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 128KB -t 100
+iperf3 -c c10-35.sysnet.ucsd.edu -w 256KB -t 100
+set +ex
