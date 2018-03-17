84fb5c906a3b3dcb5679d6e8320edfd03e086803
Jeyavaishnavi Muralikumar <jmuralik@c10-36.sysnet.ucsd.edu>
2018-03-16 23:00:43 -0700

    udp tests varying window size rather than buffer length

--- scripts/run_udp_datagram_size_tests.sh
+++ scripts/run_udp_datagram_size_tests.sh
@@ -5,12 +5,12 @@
 
 
 set -ex
-iperf3 -c $1 -u -b -l 2KB -t 60 
-iperf3 -c $1 -u -b -l 4KB -t 60
-iperf3 -c $1 -u -b -l 8KB -t 60
-iperf3 -c $1 -u -b -l 16KB -t 60
-iperf3 -c $1 -u -b -l 32KB -t 60
-iperf3 -c $1 -u -b -l 64KB -t 60
-iperf3 -c $1 -u -b -l 128KB -t 60
-iperf3 -c $1 -u -b -l 256KB -t 60
+iperf3 -c $1 -u -b -l 8192 -w 2KB -t 60 
+iperf3 -c $1 -u -b -l 8192 -w 4KB -t 60
+iperf3 -c $1 -u -b -l 8192 -w 8KB -t 60
+iperf3 -c $1 -u -b -l 8192 -w 16KB -t 60
+iperf3 -c $1 -u -b -l 8192 -w 32KB -t 60
+iperf3 -c $1 -u -b -l 8192 -w 64KB -t 60
+iperf3 -c $1 -u -b -l 8192 -w 128KB -t 60
+iperf3 -c $1 -u -b -l 8192 -w 256KB -t 60
 set +ex
